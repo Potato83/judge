8f41932840ff3c258b929a28341a47f1ee6744e8
Sam Vipond <sammy.potato@gmail.com>
2017-06-28 17:48:53 -0300

    first commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,26 @@
+import RevealOnScroll from './modules/RevealOnScroll';
+import './modules/Scroller';
+import $ from 'jquery';
+// import jQuery from 'jquery';
+
+
+//import './modules/Fader';
+
+import './modules/Test';
+
+//import Parallax from 'parallax-scroll';
+
+//const parallax = new Parallax('.js-parallax', {
+//  speed: 0.5, // Anything over 0.5 looks silly 
+//});
+//parallax.animate();
+
+// new RevealOnScroll($('.box'), "80%");
+// new RevealOnScroll($('.text'), "95%");
+// new RevealOnScroll($('.slide'), "70%");
+
+
+
+
+
+
---
+++ app/assets/scripts/Vendor.js
@@ -0,0 +1,14 @@
+import '../../temp/scripts/modernizr';
+import 'picturefill';
+import 'lazysizes';
+// const jQuery = import $ from ('jquery');
+// import jQuery from 'jquery';
+// window.$ = window.jQuery = require('jquery');
+import {$,jQuery} from 'jquery';
+// export for others scripts to use
+// window.$ = $;
+// window.jQuery = jQuery;
+// import 'bootstrap';
+
+
+
