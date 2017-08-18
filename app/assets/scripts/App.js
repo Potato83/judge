c50b56a31633df4c115f383cf8b2dbc376c045c7
Sam Vipond <sammy.potato@gmail.com>
2017-08-18 17:47:19 -0400

    add fancyFade

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,38 +1,9 @@
 import RevealOnScroll from './modules/RevealOnScroll';
 import './modules/Scroller';
 import $ from 'jquery';
-// import jQuery from 'jquery';
-
-
-//import './modules/Fader';
-
+import './modules/Fader';
 import './modules/Test';
-// import './modules/X-lightbox';
-// import 'blueimp-gallery';
 
-
-//import Parallax from 'parallax-scroll';
-
-//const parallax = new Parallax('.js-parallax', {
-//  speed: 0.5, // Anything over 0.5 looks silly 
-//});
-//parallax.animate();
-
-// new RevealOnScroll($('.box'), "80%");
-// new RevealOnScroll($('.text'), "95%");
-// new RevealOnScroll($('.slide'), "70%");
-
-
-// document.getElementById('links').onclick = function (event) {
-// 		console.log('hooowee');
-//     event = event || window.event;
-//     var target = event.target || event.srcElement,
-//         link = target.src ? target.parentNode : target,
-//         options = {index: link, event: event},
-//         links = this.getElementsByTagName('a');
-//     blueimp.Gallery(links, options);
-
-// };
 
 
 
--- app/assets/scripts/modules/Fader.js
+++ app/assets/scripts/modules/Fader.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 
 $(document).ready(function() {
  
-	var $bg = $('.o-banner__bg');
+	var $bg = $('.carousel');
 	var backgroundImage = window.getComputedStyle($bg[0], null).backgroundImage;
 	var image = backgroundImage.match(/url\((.*?)\)/)[1];
 			image = image.replace(/"/g, "");
