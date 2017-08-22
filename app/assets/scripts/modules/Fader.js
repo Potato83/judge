0cd391a1e226192424f2b16f771acc9650e861ef
Sam Vipond <sammy.potato@gmail.com>
2017-08-22 11:00:49 -0400

    fader for contact page

--- app/assets/scripts/modules/Fader.js
+++ app/assets/scripts/modules/Fader.js
@@ -1,14 +1,15 @@
 import $ from 'jquery';
 
 $(document).ready(function() {
- 
-	var $bg = $('.carousel');
+
+	var $bg = $('.fader');
 	var backgroundImage = window.getComputedStyle($bg[0], null).backgroundImage;
 	var image = backgroundImage.match(/url\((.*?)\)/)[1];
 			image = image.replace(/"/g, "");
 			var sprite = new Image();
 			sprite.src = image;
    			sprite.onload = function() {
-   				$bg.addClass('dumbo');
-				}				
+   				$bg.addClass('faded');
+				}	
+	
 });
